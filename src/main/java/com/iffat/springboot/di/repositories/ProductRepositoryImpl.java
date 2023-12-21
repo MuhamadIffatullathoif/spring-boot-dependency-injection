cc7bdb9b095f1463391b5cf196d5bfc5450dc4bf
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-22 06:09:13 +0700

    13. Request Scope annotation

--- src/main/java/com/iffat/springboot/di/repositories/ProductRepositoryImpl.java
+++ src/main/java/com/iffat/springboot/di/repositories/ProductRepositoryImpl.java
@@ -4,11 +4,13 @@ import com.iffat.springboot.di.models.Product;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Primary
+@RequestScope
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
     private List<Product> data;
