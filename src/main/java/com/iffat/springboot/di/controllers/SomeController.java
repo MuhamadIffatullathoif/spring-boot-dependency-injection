0174e7a4eddec93057e04baac64ee93dd6493dae
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-21 13:24:41 +0700

    04. Creating controller class

---
+++ src/main/java/com/iffat/springboot/di/controllers/SomeController.java
@@ -0,0 +1,27 @@
+package com.iffat.springboot.di.controllers;
+
+import com.iffat.springboot.di.models.Product;
+import com.iffat.springboot.di.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class SomeController {
+
+    private ProductService productService = new ProductService();
+
+    @GetMapping
+    public List<Product> list() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+}
