bbb6e76b5c1fb796bee971388f1a1882e3579436
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-21 12:17:35 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springboot/di/SpringBootDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springboot.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDependencyInjectionApplication.class, args);
+	}
+
+}
