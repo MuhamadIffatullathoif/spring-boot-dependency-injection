81d3aa86de28b485f4a606fc8593a69796626034
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-21 13:06:46 +0700

    02. Creating model and repository class

---
+++ src/main/java/com/iffat/springboot/di/repositories/ProductRepository.java
@@ -0,0 +1,23 @@
+package com.iffat.springboot.di.repositories;
+
+import com.iffat.springboot.di.models.Product;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductRepository {
+    private List<Product> data;
+
+    public ProductRepository() {
+        this.data = Arrays.asList(
+                new Product(1L, "Mouse", 200L),
+                new Product(2L, "Keyboard", 300L),
+                new Product(3L, "Monitor", 1000L),
+                new Product(4L, "Motherboard", 1500L)
+        );
+    }
+
+    public List<Product> findAll() {
+        return data;
+    }
+}
