3e4fb7953b8dc526f67c1e2ad76d3c5099b581bc
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-22 07:10:07 +0700

    17. Some variants Registering component annotated configuration and bean

--- src/main/java/com/iffat/springboot/di/AppConfig.java
+++ src/main/java/com/iffat/springboot/di/AppConfig.java
@@ -1,16 +1,21 @@
 package com.iffat.springboot.di;
 
 import com.iffat.springboot.di.repositories.ProductRepositoryJson;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
 
 @Configuration
 @PropertySource("classpath:config.properties")
 public class AppConfig {
 
+    @Value("classpath:json/product.json")
+    private Resource resource;
+
     @Bean
     ProductRepositoryJson productRepositoryJson(){
-        return new ProductRepositoryJson();
+        return new ProductRepositoryJson(resource);
     }
 }
--- src/main/java/com/iffat/springboot/di/repositories/ProductRepositoryJson.java
+++ src/main/java/com/iffat/springboot/di/repositories/ProductRepositoryJson.java
@@ -11,12 +11,20 @@ import java.util.Arrays;
 import java.util.List;
 
 @Repository
-public class ProductRepositoryJson implements ProductRepository{
+public class ProductRepositoryJson implements ProductRepository {
 
     private List<Product> list;
 
     public ProductRepositoryJson() {
         Resource resource = new ClassPathResource("json/product.json");
+        readValueJson(resource);
+    }
+
+    public ProductRepositoryJson(Resource resource) {
+        readValueJson(resource);
+    }
+
+    private void readValueJson(Resource resource) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
